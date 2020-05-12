2ac5f59f721eb990dd59ee24a9db314cf20ab563
Andy <38047996+ZzzandyzzZ@users.noreply.github.com>
2020-05-12 12:20:19 -0500

    update

--- Histogram Equalization/Pruebas/Histogram equalization.py
+++ Histogram Equalization/Pruebas/Histogram equalization.py
@@ -3,8 +3,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 import math as mt
 img = cv2.imread("img2.jpg",0)
-hist = cv2.calcHist([img[250:400,180:250]],[0],None,[256],[0,256])
-cv2.imshow("corte",img[250:400,180:250])
+hist = cv2.calcHist([img],[0],None,[256],[0,256])
+#Histograma de una parte
+#hist = cv2.calcHist([img[250:400,180:250]],[0],None,[256],[0,256])
 P=[i/img.size for i in hist]
 S=[mt.floor(sum(P[0:i+1])*256) for i in range(0,len(P))]
 for i in range(0,img.shape[0]):
@@ -12,7 +13,5 @@ for i in range(0,img.shape[0]):
         img[i,j]=S[img[i,j]]
 hist = cv2.calcHist([img],[0],None,[256],[0,256])
 plt.plot(hist)
-
 cv2.imshow("output",img)
-
 plt.show()
