a79f7ce2826d11149179265f6e99e3154b804ad4
EduardoSanchezH25 <esanchezh@unsa.edu.pe>
2020-05-22 03:07:59 -0500

    Revert "CORTAR"
    This reverts commit e6ce87862389d1e8504be67afddf27ad095aefab.

--- grafi/index.php
+++ grafi/index.php
@@ -7,35 +7,8 @@
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/ajax.js"></script>
 	<script type="text/javascript" src="js/funciones.js"></script>
-  <link rel='stylesheet' href='css/imgareaselect-animated.css'>
-  <link rel='stylesheet' href='css/imgareaselect-default.css'>
 	<meta charset="utf-8">
 
-
-<meta charset="utf-8">
-  <title>Swiper demo</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-  <!-- Link Swiper's CSS -->
-  <link rel="stylesheet" href="https://swiperjs.com/package/css/swiper.min.css">
-
-  <!-- Demo styles -->
-  <style>
-    html, body {
-      position: relative;
-      height: 100%;
-    }
-    body {
-      margin: 0;
-      padding: 0;
-    }
-    .swiper-container {
-      width: 100%;
-      height: 70%;
-    }
-    .swiper-slide {
-      overflow: hidden;
-    }
-  </style>
 </head>
 <body>
 	<div class="container">
@@ -63,90 +36,27 @@
 	</div>
 	</form>
 	</div>
-
-
-   <div class="container">
-     <p>&nbsp;</p>
-    <input type="button" id="boton_de_recorte" value="Recortar" >
-    <p>&nbsp;</p>
-   </div>
-   
-     <center>
-  <figure>
-    <div id="divid">
-        <?php
-
-            if(file_exists("uploads/original.jpg")){
-              echo '<img src="uploads/original.jpg?'.rand().'" id="mi_imagen" alt="">';
-            }
-          ?> 
-     </div>  
-      </figure>
-  </center>
-  
-  <div class="swiper-container">
-    <div class="swiper-wrapper">
-     
-      <div class="swiper-slide">
-        <div class="swiper-zoom-container">
+	<div class="container">
+		<div class="row align-items-center">
+			<div id="divid" name="divid">
 				<?php
 					if(file_exists("uploads/original.jpg")){
-					echo '<img src="uploads/histograma.png?'.rand().'">';
+
+						$direc = '<img src="uploads/original.jpg?'.rand().'" class="img-fluid" alt="Responsive image">';
+						$direc2 = '<img align="right" src="uploads/histograma.png?'.rand().'" class="img-fluid" alt="Responsive image">';
+					echo $direc.$direc2;
 					}
 				?>
-        </div>
-      </div>
-      
-      <?php
-
-            if(file_exists("uploads/original.jpg")){
-              echo '<div class="swiper-slide">
-              <div class="swiper-zoom-container"> 
-              <img src="uploads/recorte.jpg?'.rand().'" id="mi_imagen" alt=""> 
-                </div> </div>';
-            }
-          ?>  
-
-      <div class="swiper-slide">
-        <div class="swiper-zoom-container">
-         <div id="dividR" name="dividR">
-          <?php
-			if(file_exists("uploads/original.jpg")){
-				echo '<img src="uploads/resultado.jpg?'.rand().'">';
-			}
-		?>
-       </div>
-        </div>
-      </div>
-    </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination swiper-pagination-white"></div>
-    <!-- Add Navigation -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-  </div>
-
-  <!-- Swiper JS -->
-  <script src="https://swiperjs.com/package/js/swiper.min.js"></script>
-
-  <!-- Initialize Swiper -->
-  <script>
-    var swiper = new Swiper('.swiper-container', {
-      zoom: true,
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  </script>
-
-  <!-- Recorte -->
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="js/jquery.imgareaselect.js"></script>
-  <script type="text/javascript" src="js/recorte.js"></script>
-
+			</div>
+			<div id="dividR" name="dividR">
+				<?php
+					if(file_exists("uploads/resultado.jpg")){
+						$direc = '<img src="uploads/resultado.jpg?'.rand().'" class="img-fluid" alt="Responsive image">';
+					echo $direc;
+					}
+				?>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
