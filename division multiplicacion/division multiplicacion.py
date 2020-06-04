27888de55fd2c52364f2b7f82d08d8484e211ee4
Andy <38047996+ZzzandyzzZ@users.noreply.github.com>
2020-06-04 12:51:26 -0500

    Update division multiplicacion.py

--- division multiplicacion/division multiplicacion.py
+++ division multiplicacion/division multiplicacion.py
@@ -7,7 +7,7 @@ img = cv2.imread("sub_10.jpg",0)
 img2 = cv2.imread("sub_11.jpg",0)
 img2=cv2.resize(img2,(img.shape[1],img.shape[0]))
 
-
+#EJERCICIO 3
 for i in range(img2.shape[0]):
     for j in range(img2.shape[1]):
         img[i,j]=int(img[i,j])/int(img2[i,j])*30
