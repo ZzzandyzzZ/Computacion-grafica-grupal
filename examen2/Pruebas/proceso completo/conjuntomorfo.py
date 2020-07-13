ea49f7c7ab928250a97c47bfb3e09fe6f625baa1
lvillanuevaf@unsa.edu.pe <lvillanuevaf@unsa.edu.pe>
2020-07-13 15:57:53 -0500

    Actualización operadores

--- examen2/Pruebas/proceso completo/conjuntomorfo.py
+++ examen2/Pruebas/proceso completo/conjuntomorfo.py
@@ -65,6 +65,35 @@ def conjunto(img,kernel):
     r=dilation(r,kernel)
     return r
 
+def esquinas(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(gray, 4, 0.01, 200)
+    corners = np.int0(corners)
+    f = open ('puntos.txt','w')
+    p1x=str(corners[0][0][0])
+    p2x=str(corners[1][0][0])
+    p3x=str(corners[2][0][0])
+    p4x=str(corners[3][0][0])
+    p1y=str(corners[0][0][1])
+    p2y=str(corners[1][0][1])
+    p3y=str(corners[2][0][1])
+    p4y=str(corners[3][0][1])
+    f.write(p1x)
+    f.write(",")
+    f.write(p1y)
+    f.write(",")
+    f.write(p2x)
+    f.write(",")
+    f.write(p2y)
+    f.write(",")
+    f.write(p3x)
+    f.write(",")
+    f.write(p3y)
+    f.write(",")
+    f.write(p4x)
+    f.write(",")
+    f.write(p4y)
+    f.close()
 
 
 
@@ -75,6 +104,8 @@ img=cv2.resize(img,(500,500))
 kernel=np.ones((5,5),np.uint8)
 reso=conjunto(img,kernel)
 cv2.imwrite('resultado.png',reso)
+img3=cv2.imread('resultado.png')
+esquinas(img3)
 
 
 '''
