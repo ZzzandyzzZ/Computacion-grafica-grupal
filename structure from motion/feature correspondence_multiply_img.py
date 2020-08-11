be2db5b6dd8a4912fb4414bce743cd472361fdba
ZzzandyzzZ <andy_gean@hotmail.com>
2020-08-11 14:23:03 -0500

    dado

--- structure from motion/feature correspondence_multiply_img.py
+++ structure from motion/feature correspondence_multiply_img.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 path="dataset_modified/"
-"""
+
 n_images=6
 img_matches=[]
 def compute_images(name,n_images,size):
@@ -18,16 +18,22 @@ def compute_images(name,n_images,size):
         kp1, des1 = sift.detectAndCompute(img1,None)
         kp2, des2 = sift.detectAndCompute(img2,None)
         matches = bf.knnMatch(des1,des2, k=2)
+        """
+        matches = bf.match(des1,des2)
+        matches = sorted(matches, key = lambda x:x.distance)
+        good=matches[:15]
+        """
         good = []
         for m,n in matches:
-            if m.distance < 0.75*n.distance:
-                good.append([m])
+            if m.distance < 0.8*n.distance:
+                good.append([m])       
         img3=None if(i==0) else img_matches[i-1]
+        #img_matches.append(cv.drawMatches(img1,kp1,img2,kp2,good,img3,flags=2))
         img_matches.append(cv.drawMatchesKnn(img1,kp1,img2,kp2,good,img3,flags=2))
-        cv.imwrite('sift_keypoints_{}.jpg'.format(i),img_matches[i])
+        cv.imshow('sift_keypoints_{}_2.jpg'.format(i),img_matches[i])
 
-img =compute_images("tasa",6,(650,650))
-cv.imwrite('sift_keypoints.jpg',img_matches[3])
+img =compute_images("dado",2,(650,650))
+#cv.imwrite('sift_keypoints.jpg',img_matches[3])
 """
 img1 = cv.imread(path+'tasa (2).jpg')
 img2 = cv.imread(path+'tasa (3).jpg')
@@ -49,6 +55,7 @@ for m,n in matches:
         good.append([m])
 img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 matches = bf.knnMatch(des2,des3, k=2)
+#matches = sorted(matches, key = lambda x:x.distance)
 good = []
 for m,n in matches:
     if m.distance < 0.75*n.distance:
@@ -59,3 +66,4 @@ cv.imwrite('sift_keypoints.jpg',img4)
 ##img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 ##cv.imshow('sift_keypoints.jpg',img)
 
+"""
