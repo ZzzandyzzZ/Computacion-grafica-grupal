8c6c0ec2ccb9198ab5c758198ac1481a1305a27b
ZzzandyzzZ <andy_gean@hotmail.com>
2020-08-11 14:36:44 -0500

    update

--- structure from motion/feature correspondence.py
+++ structure from motion/feature correspondence.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2 as cv
 path="dataset_modified/"
-img1 = cv.imread(path+'tasa (2).jpg')
-img2 = cv.imread(path+'tasa (3).jpg')
+img1 = cv.imread(path+'dado (3).jpg')
+img2 = cv.imread(path+'dado (4).jpg')
 img1=cv.resize(img1,(650,650))
 img2=cv.resize(img2,(650,650))
 ##gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
@@ -14,7 +14,7 @@ bf = cv.BFMatcher()
 matches = bf.knnMatch(des1,des2, k=2)
 good = []
 for m,n in matches:
-    if m.distance < 0.75*n.distance:
+    if m.distance < 0.5*n.distance:
         good.append([m])
 img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 cv.imshow('sift_keypoints.jpg',img3)
