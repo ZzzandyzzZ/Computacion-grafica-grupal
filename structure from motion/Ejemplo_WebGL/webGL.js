53a1dec67c7348950c5ed42a0becb967f14fcf81
Eduardo Sanchez <esanchezh@unsa.edu.pe>
2020-08-18 12:37:26 -0500

    WebGL

--- structure from motion/Ejemplo_WebGL/webGL.js
+++ structure from motion/Ejemplo_WebGL/webGL.js
@@ -1,76 +1,65 @@
 /******************************* variables *******************/
 //Preparamos el render
-var Render=new THREE.WebGLRenderer();
+var Render = new THREE.WebGLRenderer();
 //El escenario
-var Escenario=new THREE.Scene();
+var Escenario = new THREE.Scene();
 //La cámara
-var Camara=new THREE.PerspectiveCamera();
-// la Figura 
+var Camara = new THREE.PerspectiveCamera();
+// la Figura
 var Figura;
+//var sphere;
 var controls;
 
 /**************************llamado a las funciones ******************/
 inicio();
 animacion();
 /******************************* inicio *******************/
-function inicio(){
-       //Tamaño del render(resultado)
-       Render.setSize(800,600);
-       //Se agrega el render al documento html
-       document.getElementById('render').appendChild(Render.domElement);
-       //Acercamos la cámara en z es profundidad para ver el punto
-       Camara.position.z=100;
-       //agregando la cámara al escenario
-       Escenario.add(Camara);
-         // cargar nuevos modelos
-       cargar_modelo();
-       // agregamos todo el escenario y la cámara al render
-        controls=new THREE.OrbitControls(Camara,Render.domElement);
+function inicio() {
+  //Tamaño del render(resultado)
+  Render.setSize(800, 600);
+  //Se agrega el render al documento html
+  document.getElementById("render").appendChild(Render.domElement);
+  //Acercamos la cámara en z es profundidad para ver el punto
+  Camara.position.z = 100;
+  //agregando la cámara al escenario
+  Escenario.add(Camara);
+  // cargar nuevos modelos
+  cargar_modelo();
+  // agregamos todo el escenario y la cámara al render
+  controls = new THREE.OrbitControls(Camara, Render.domElement);
 }
-function cargar_modelo(){
-    // Geometría
-    Geometria=new THREE.Geometry();
-    var vertices=[
-                    [2,7,0],
-                    [7,2,0],
-                    [12,7,0],
-                    [12,17,0],
-                    [7,12,0],
-                    [2,17,0],
-                    [2,7,0],
-                    [2,7,2],
-                    [7,2,2],
-                    [12,7,2],
-                    [12,17,2],
-                    [7,12,2],
-                    [2,17,2],
-                    [2,7,2]
-    
-                ];
-    var long_vertices=vertices.length;
-    for(i=0;i<long_vertices;i++){
-        x=vertices[i][0];
-        y=vertices[i][1];
-        z=vertices[i][2];
-        //Agregamos vértices al vector
-        Vector=new THREE.Vector3(x,y,z);
-        //Agregamos el vector a la geometría
-        Geometria.vertices.push(Vector);   
-    }
-    // agregamos un material para que el punto tenga color
-    Material=new THREE.ParticleBasicMaterial({color:0XFF0000});
-    // creamos una partícula con la geometría y el material
-    Figura=new THREE.Line(Geometria,Material);
-    // agregamos la partícula al escenario
-    Escenario.add(Figura);
+function cargar_modelo() {
+  //////////////////////
+  var geometry;
+  var material;
+  var sphere;
+  /////////////////////
+  var file = "coordenadas.txt";
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.send(null);
+  var txt = rawFile.responseText;
+  var xD = txt.split("\n");
+  var x;
+  for (var i = 0; i < xD.length; i++) {
+    x = xD[i].split(",");
+    geometry = new THREE.SphereGeometry(1, 8, 6);
+    material = new THREE.MeshBasicMaterial({
+      color: 0xffff00,
+      wireframe: true,
+    });
+    sphere = new THREE.Mesh(geometry, material);
+    sphere.position.x = x[0];
+    sphere.position.y = x[1];
+    sphere.position.z = x[2];
+    Escenario.add(sphere);
+  }
 }
-function animacion(){
-    requestAnimationFrame(animacion);
-    render_modelo();
+function animacion() {
+  requestAnimationFrame(animacion);
+  render_modelo();
 }
-function render_modelo(){
-    controls.update();
-       Render.render(Escenario,Camara);
-    }
-
-
+function render_modelo() {
+  controls.update();
+  Render.render(Escenario, Camara);
+}
